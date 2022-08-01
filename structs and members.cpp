b7d9ffa41f1b31d554ac1f792e534498eb1e0a0f
re48jr7q <erwin.degeus45@gmail.com>
2022-08-01 17:33:26 +0200

    Add project files.

---
+++ structs and members.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "structs and members.h"
+
+
+
+
+int main()
+{
+    
+}
+
---
+++ structs and members.h
@@ -0,0 +1,19 @@
+#ifndef STRUCTS_AND_MEMBERS
+#define STRUCTS_AND_MEMBERS
+
+std::string name;
+std::string title;
+
+int age;
+int id;
+int managerId;
+double wage;
+int birthdayYear;
+int birthdayMonth;
+int birthdayDay;
+int hireYear;
+int hireMonth;
+int hireDay;
+
+
+#endif
